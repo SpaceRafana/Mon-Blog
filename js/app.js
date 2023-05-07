bcb149e6cc7375a016f8f2ae7af455bf60021f6c
unknown <fanekena.f12@gmail.com>
2023-05-07 17:15:03 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    //icon menu
+    $(".icon-menu button").on("click", function () {
+        $(".nav-links ul").toggleClass("open");
+        $(".nav-links .icon-menu i").toggleClass("fa-arrow-right").toggleClass("fa-ellipsis-h");
+    });
+    //---------
+
+    //liste menu and mobile
+    var $slide = $("#slide"),
+        $ul = $(".nav-links ul"),
+        $icon_menu = $(".nav-links .icon-menu i"),
+        $mPortfolio = $("#menu-portfolio"),
+        $mAccueil = $("#menu-accueil");
+
+    $(".nav-links #menu-portfolio").on("click", function () {
+        $slide.addClass("in").removeClass("out");
+        $(".portfolio").fadeToggle(700);
+        $ul.toggleClass("open");
+        //icon change
+        $icon_menu.toggleClass("fa-arrow-right").toggleClass("fa-ellipsis-h");
+
+        //menu active
+        $mPortfolio.addClass("active");
+        $mAccueil.removeClass("active");
+    });
+
+    $(".nav-links #menu-accueil").on("click", function () {
+        $slide.removeClass("in").addClass("out");
+        $(".portfolio").fadeToggle(700);
+        $ul.toggleClass("open");
+        //icon change
+        $icon_menu.toggleClass("fa-arrow-right").toggleClass("fa-ellipsis-h");
+
+        //menu active
+        $mAccueil.addClass("active");
+        $mPortfolio.removeClass("active");
+
+    });
+    //---------
+
+    //animation accueil
+    $(".accueil .presentation .bonjour").each(function () {
+        $(this).css({opacity: "0%"});
+        $(this).animate({opacity: "100%"}, 900);
+    });
+    $(".accueil .presentation .moi").each(function () {
+        $(this).css({position: "relative", opacity: "0%", top: 10}).delay(900);
+        $(this).animate({opacity: "100%", top: 0}, 1000);
+    });
+    $(".accueil .presentation h5").each(function () {
+        $(this).css({position:"relative", opacity: "0%", top: 10}).delay(1200);
+        $(this).animate({opacity: "100%", top: 0 }, 1000);
+    });
+    $(".accueil .presentation button").each(function () {
+        $(this).delay(2500);
+        $(this).animate({opacity: "100%"}, 1000);
+    });
+    //---------
+
+
+});
