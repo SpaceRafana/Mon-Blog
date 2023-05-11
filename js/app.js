7da4264fccd568ed32ff6373811a91297f15e25c
unknown <fanekena.f12@gmail.com>
2023-05-11 21:45:12 +0200

    updated

--- js/app.js
+++ js/app.js
@@ -6,54 +6,65 @@ $(document).ready(function () {
     });
     //---------
 
+    /*scroll to top*/
+
+    $(window).on('scroll', function (){
+        scroll = $(window).scrollTop();
+        if (scroll >= 100){
+            $("#back-to-top").addClass('b-show_scrollBut');
+        }else{
+            $("#back-to-top").removeClass('b-show_scrollBut');
+        }
+    });
+    $("#back-to-top").on("click", function(){
+        $('body,html').animate({
+            scrollTop: 0,
+        }, 100);
+    });
+
+    //------------------
+
     //liste menu and mobile
     var $slide = $("#slide"),
         $ul = $(".nav-links ul"),
         $icon_menu = $(".nav-links .icon-menu i"),
-        $mPortfolio = $("#menu-portfolio"),
-        $mAccueil = $("#menu-accueil");
+        $btnRetourAccueil = $(".portfolio .retour-accueil button"),
+        $mAccueil = $("#menu-accueil"),
+        $mArticle = $("#menu-article");
 
-    $(".nav-links #menu-portfolio").on("click", function () {
+    $(".presentation button").on("click", function () {
         $slide.addClass("in").removeClass("out");
-        $(".portfolio").fadeToggle(700);
-        $ul.toggleClass("open");
-        //icon change
-        $icon_menu.toggleClass("fa-arrow-right").toggleClass("fa-ellipsis-h");
-
-        //menu active
-        $mPortfolio.addClass("active");
-        $mAccueil.removeClass("active");
     });
 
-    $(".nav-links #menu-accueil").on("click", function () {
+    $btnRetourAccueil.on("click", function () {
         $slide.removeClass("in").addClass("out");
-        $(".portfolio").fadeToggle(700);
+    });
+
+    $(".nav-links #menu-article").on("click", function () {
+        $slide.removeClass("in").addClass("out");
         $ul.toggleClass("open");
         //icon change
         $icon_menu.toggleClass("fa-arrow-right").toggleClass("fa-ellipsis-h");
 
         //menu active
-        $mAccueil.addClass("active");
-        $mPortfolio.removeClass("active");
+        $mArticle.addClass("active");
+        $mAccueil.removeClass("active");
 
     });
+
     //---------
 
     //animation accueil
     $(".accueil .presentation .bonjour").each(function () {
-        $(this).animate({opacity: 1}, 900);
+        $(this).delay(500).fadeToggle(1000);
     });
     $(".accueil .presentation .moi").each(function () {
-        $(this).css({top: 10}).delay(900);
+        $(this).css({top: 10}).delay(800);
         $(this).animate({opacity: 1, top: 0}, 1000);
     });
     $(".accueil .presentation h2").each(function () {
         $(this).css({top: 10}).delay(1200);
         $(this).animate({opacity: "100%", top: 0 }, 1000);
-    });
-    $(".accueil .presentation a").each(function () {
-        $(this).delay(2500);
-        $(this).animate({opacity: "100%"}, 1000);
     });
     //---------
 
